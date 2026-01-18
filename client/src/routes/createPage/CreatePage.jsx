9b84db3a605cff39f376bb808db2ddca76c3445a
stwhello <sherinpaul2001@gmail.com>
2026-01-18 20:53:26 +0530

    create page completed

--- client/src/routes/createPage/CreatePage.jsx
+++ client/src/routes/createPage/CreatePage.jsx
@@ -1,9 +1,75 @@
-import "./createPage.css"
+import ImageN from "../../components/imageN/ImageN";
+import "./createPage.css";
 
 const CreatePage = () => {
   return (
-    <div className="createPage">CreatePage</div>
-  )
-}
+    <div className='createPage'>
+      <div className='createTop'>
+        <h1>Create Pin</h1>
+        <button>Publish</button>
+      </div>
+      <div className='createBottom'>
+        <div className='upload'>
+          <div className='uploadTitle'>
+            <ImageN path='/general/upload.svg' alt='' />
+            <span>Choose a file</span>
+          </div>
+          <div className='uploadInfo'>
+            We recommend using high quality .jpg files less than 20 files less
+            than 200 MB.
+          </div>
+        </div>
+        <form className='createForm'>
+          <div className='createFormItem'>
+            <label htmlFor='title'>Title</label>
+            <input
+              type='text'
+              placeholder='Add a title'
+              name='title'
+              id='title'
+            />
+          </div>
+          <div className='createFormItem'>
+            <label htmlFor='description'>Description</label>
+            <textarea
+              rows={6}
+              type='text'
+              placeholder='Add a detailed description'
+              name='description'
+              id='description'
+            />
+          </div>
+          <div className='createFormItem'>
+            <label htmlFor='link'>Link</label>
+            <input
+              type='text'
+              placeholder='Add a link'
+              name='link'
+              id='link'
+            />
+          </div>
+          <div className='createFormItem'>
+            <label htmlFor='board'>Board</label>
+            <select name="board" id="board">
+              <option value="1">Board 1</option>
+              <option value="2">Board 2</option>
+              <option value="3">Board 3</option>
+            </select>
+          </div>
+          <div className='createFormItem'>
+            <label htmlFor='tags'>Tagged topic</label>
+            <input
+              type='text'
+              placeholder='Add tags'
+              name='tags'
+              id='tags'
+            />
+            <small>Don't worry, people won't see your tags</small>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default CreatePage
+export default CreatePage;
