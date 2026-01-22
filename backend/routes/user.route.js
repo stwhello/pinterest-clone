6378de8d52d6029f14c0e2ff7f091cba33ceffa6
stwhello <sherinpaul2001@gmail.com>
2026-01-22 20:39:18 +0530

    models created and schema defined

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,6 +3,12 @@ import { test } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/test", test)
+router.post("/create", (req, res) => {
+  const userInformation = req.body;
+
+  console.log(userInformation);
+});
+
+router.get("/test", test);
 
 export default router;
