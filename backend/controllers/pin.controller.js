7cf03c79892814154b1a5ad7234b8d1aa48796f6
stwhello <sherinpaul2001@gmail.com>
2026-01-29 08:46:08 +0530

    added infinite scrolling to homw gallery page

--- backend/controllers/pin.controller.js
+++ backend/controllers/pin.controller.js
@@ -1,7 +1,17 @@
 import Pin from "../models/pin.model.js";
 
 export const getPins = async (req, res) => {
-  const pins = await Pin.find();
+  const pageNumber = Number(req.query.cursor) || 0;
+  const LIMIT = 21;
+  const pins = await Pin.find()
+    .limit(LIMIT)
+    .skip(pageNumber * LIMIT);
 
-  res.status(200).json(pins);
+  const hasNextPage = pins.length === LIMIT;
+
+  await new Promise(resolve=>setTimeout(resolve,3000))
+
+  res
+    .status(200)
+    .json({ pins, nextCursor: hasNextPage ? pageNumber + 1 : null });
 };
--- client/src/components/galleryItem/GalleryItem.jsx
+++ client/src/components/galleryItem/GalleryItem.jsx
@@ -12,7 +12,7 @@ const GalleryItem = ({ item }) => {
     >
       {/* <img src={item.media} alt='' /> */}
       <ImageN path={item.media} alt="" w={372} h={optimizedHeight}/>
-      <Link to={`/pin/${item.id}`} className='overlay' />
+      <Link to={`/pin/${item._id}`} className='overlay' />
       <button className='saveButton'>Save</button>
       <div className='overlayIcons'>
         <button>
