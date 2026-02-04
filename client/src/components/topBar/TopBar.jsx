4404d5bd6632dbc94c7bdb5cfa0ea30e8b5c9b96
stwhello <sherinpaul2001@gmail.com>
2026-02-04 17:31:21 +0530

    added infinite scrolling to homw gallery page

--- client/src/components/topBar/TopBar.jsx
+++ client/src/components/topBar/TopBar.jsx
@@ -1,20 +1,24 @@
-import ImageN from "../imageN/ImageN"
-import UserButton from "../userButton/UserButton"
-import "./topBar.css"
+import ImageN from "../imageN/ImageN";
+import UserButton from "../userButton/UserButton";
+import "./topBar.css";
 
 const TopBar = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="topBar">
+    <div className='topBar'>
       {/* SEARCH */}
-      <div className="search">
-        <ImageN path="/general/search.svg" alt="" />
-        <input type="text"placeholder="Search" />
-      </div>
+      <form onSubmit={handleSubmit} className='search'>
+        <ImageN path='/general/search.svg' alt='' />
+        <input type='text' placeholder='Search' />
+      </form>
 
       {/* USER */}
       <UserButton />
     </div>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
